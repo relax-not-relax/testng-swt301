307906be596e65a4434c3fc2a838d99712646fe7
hanh <hoanganhtranthi1610@gmail.com>
2022-10-22 09:43:02 +0700

    complete param test

---
+++ math-param-ant/src/com/hanh/paramTestNG/core/GetMaxOccurringChar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hanh.paramTestNG.core;
+
+/**
+ *
+ * @author Hoang Anh
+ */
+public class GetMaxOccurringChar {
+     public static char getMaxOccurringChar(String str)       
+    {
+        int counter[] = new int[256];
+        String sentences=str.toLowerCase();
+        int len = sentences.length();
+        for (int i = 0; i < len; i++)
+            counter[sentences.charAt(i)]++;
+        int max= counter[sentences.charAt(0)];
+        char c=sentences.charAt(0);
+        char array[] = new char[sentences.length()];
+        for (int i = 0; i < len; i++) {
+            array[i] = sentences.charAt(i);
+            int flag = 0;
+            for (int j = 0; j <= i; j++) {
+                if (sentences.charAt(i) == array[j])
+                    flag++;
+            }
+            if (flag == 1)         
+        if (max < counter[sentences.charAt(i)]){
+            max = counter[sentences.charAt(i)];
+            c=sentences.charAt(i);
+            System.out.println(c);
+        }
+        }
+        return c;
+    }   
+}
